4dce4ad00b7c22ca135afaf99682ba37feedd0c8
Jay Ess <mentoce@gmail.com>
2019-04-21 13:45:02 -0700

    Update comments

--- labs/lab02/ArraySet.h
+++ labs/lab02/ArraySet.h
@@ -23,13 +23,12 @@ public:
         elements = nullptr;
     }
     /** Adds an element to the set, if possible
-     @post the element is added to the set and the number of elements is
+     @post The element is added to the set and the number of elements is
         increased by one (if successful)
-     @param elem the object to be added as a new element of the set
+     @param elem The object to be added as a new element of the set
      @return true if the element was added, false if it wasn't */
     bool add(const T &elem)
     {
-        // bool isIn = ;
         bool canAdd = (numElements < maxElements) && !isElement(elem);
         if (canAdd)
         {
@@ -41,13 +40,13 @@ public:
     }
 
     /** Returns the the intersection of this set and aSet
-     @param aSet the set to form an intersection with this set
+     @param aSet The set to form an intersection with this set
      @return The integer number of elements in the set */
     void intersection(const SetInterface<T> &aSet, SetInterface<T> &interSet)
     { /* NOT IMPLEMENTED*/}
 
     /** Checks if an object is an element to the set
-     @param elem the object to be checked if it is an element of the set
+     @param elem The object to be checked if it is an element of the set
      @return true if elem is an element of this set, false if not */
     bool isElement(const T &elem)
     {
@@ -73,7 +72,6 @@ public:
     bool isSubset(const SetInterface<T> &aSet)
     {
         std::vector<T> aVector = aSet.vector();
-        // std::vector<T> thisVector = vector();
 
         for (auto const &aElem : aVector)
         {
@@ -87,17 +85,16 @@ public:
     }
 
     /** Removes the object from the set, if it is present
-     @post the element is removed from the set and the number of elements is
+     @post The element is removed from the set and the number of elements is
         decreased by one (if successful)
-     @param elem the object to be removed as a new element of the set
-     true if the element was removed, false if it wasn't */
+     @param elem The object to be removed from the set
+     @return true if the element was removed, false if it wasn't */
     bool remove(const T &elem)
     {
-        // compare each element in the elements array to
-        // elem. If a match is found, set the element at
-        // the matching index to the last element in the
-        // elements array and decrease numElements, and
-        // immediately return true
+        // compare each element in the elements array to elem. If a match is
+        // found, set the element at the matching index to the last element in
+        // the elements array and decrease numElements, and immediately return
+        // true
         for (int i = 0; i < numElements; i++)
         {
             if (elements[i] == elem)
@@ -109,8 +106,8 @@ public:
             }
         }
 
-        // elem could not be found in the elements array, therefore
-        // return false
+        // elem could not be found in the elements array, therefore return
+        // false
         return false;
     }
 
@@ -137,8 +134,7 @@ public:
     }
 
     /** Returns all of the elements in the set as a vector
-     @return a vector containing the elements of the set
-    */
+     @return a vector containing the elements of the set */
     std::vector<T> vector() const
     {
         std::vector<T> setVect;
