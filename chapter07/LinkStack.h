34b9611a1ceea204c736f178af8730d4d79d4a5a
jay ess <mentoce@gmail.com>
2019-04-24 15:03:42 -0700

    Start definition of link-based stack

---
+++ chapter07/LinkStack.h
@@ -0,0 +1,13 @@
+#ifndef _LINK_STACK_H
+#define _LINK_STACK_H
+
+#include "StackInterface.h"
+
+template<class T>
+class LinkStack : public StackInterface
+{
+
+};
+
+#include "LinkStack.cpp"
+#endif // _LINK_STACK_H
