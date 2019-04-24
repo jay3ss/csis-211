cfd5c780c289553a5f0c0e9d28d78d7674c6e163
jay ess <mentoce@gmail.com>
2019-04-24 14:12:32 -0700

    Add constant to limit size of the stack

--- chapter07/ArrayStack.h
+++ chapter07/ArrayStack.h
@@ -1,7 +1,9 @@
 #ifndef _ARRAY_STACK_H
 #define _ARRAY_STACK_H
 
-#include "StackInterface.h"
+#include "StackInterface.h" stack
+
+const int MAX_STACK = 1000; // maximum size of the
 
 template<class T>
 class ArrayStack : public StackInterface
