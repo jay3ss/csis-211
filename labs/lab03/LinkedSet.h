8947a511075a09c862b20b74dba3bd22290b97c5
jay ess <mentoce@gmail.com>
2019-04-30 13:56:10 -0700

    Fix formatting of javadoc comments

--- labs/lab03/LinkedSet.h
+++ labs/lab03/LinkedSet.h
@@ -20,30 +20,30 @@ public:
     /** Adds an element to the set, if possible
     @post the element is added to the set and the number of elements is
       increased by one (if successful)
-   @param elem the object to be added as a new element of the set
-   @return true if the element was added, false if it wasn't */
+    @param elem the object to be added as a new element of the set
+    @return true if the element was added, false if it wasn't */
     bool add(const T &elem) { return false; }
 
     /** Returns the the intersection of this set and aSet
     @param aSet the set to form an intersection with this set
-   @return The integer number of elements in the set */
+    @return The integer number of elements in the set */
     void intersection(const SetInterface<T> &aSet, SetInterface<T> &unionSet) {}
 
     /** Checks if an object is an element to the set
     @param elem the object to be checked if it is an element of the set
-   @return true if elem is an element of this set, false if not */
+    @return true if elem is an element of this set, false if not */
     bool isElement(const T &elem) { return false; }
 
     /** Returns the the intersection of this set and aSet
     @param aSet the set to check if it is a subset of this set
-   @return true if aSet is a subset of this set, false if not */
+    @return true if aSet is a subset of this set, false if not */
     bool isSubset(const SetInterface<T> &aSet) { return false; }
 
     /** Removes the object from the set, if it is present
     @post the element is removed from the set and the number of elements is
       decreased by one (if successful)
-   @param elem the object to be removed as a new element of the set
-   true if the element was removed, false if it wasn't */
+    @param elem the object to be removed as a new element of the set
+    true if the element was removed, false if it wasn't */
     bool remove(const T &elem) { return false; }
 
     /** Returns the integer number of elements in the set
@@ -52,7 +52,7 @@ public:
 
     /** Returns the the union of this set and aSet
     @param aSet the set to form a union with this set
-   @return The integer number of elements in the set */
+    @return The integer number of elements in the set */
     void unionSet(const SetInterface<T> &aSet, SetInterface<T> &unionSet) {}
 
     /** Returns all of the elements in the set as a vector
