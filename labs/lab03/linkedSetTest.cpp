9b0196d1edd6bc763ba2e7b0d237124964b3f778
jay ess <mentoce@gmail.com>
2019-04-30 15:00:42 -0700

    Add test to add elements to set

--- labs/lab03/linkedSetTest.cpp
+++ labs/lab03/linkedSetTest.cpp
@@ -16,20 +16,19 @@ int main()
          cout << "size(): returns " << intLS.size()
               << "; should be 0\n\n";
 
-     //     cout << "Add 5 elements to the set:\n";
+         cout << "Add 5 elements to the set:\n";
 
-     //     string elements[] = {"This", "is", "a", "test", "set"};
-     //     for (int i = 0; i < 5; i++)
-     //     {
-     //         set1.add(elements[i]);
-     //     }
-     //     cout << "size(): returns " << set1.size()
-     //          << "; should be 5\n\n";
+         for (int i = 0; i < 5; i++)
+         {
+             intLS.add(i);
+         }
+         cout << "size(): returns " << intLS.size()
+              << "; should be 5\n\n";
 
-     //     cout << "isElement(\"This\"): returns " << set1.isElement("This")
+     //     cout << "isElement(\"This\"): returns " << intLS.isElement(3)
      //          << "; should be 1 (true)\n\n";
 
-     //     cout << "isElement(\"C++\"): returns " << set1.isElement("C++")
+     //     cout << "isElement(\"C++\"): returns " << intLS.isElement(10)
      //          << "; should be 0 (false)\n\n";
 
      //     cout << "Create a new set that is a subset:\n";
@@ -40,7 +39,7 @@ int main()
      //         set2.add(elements[i]);
      //     }
 
-     //     cout << "isSubset(set2): returns " << set1.isSubset(set2)
+     //     cout << "isSubset(set2): returns " << intLS.isSubset(set2)
      //          << "; should return 1 (true)\n\n";
 
      //     cout << "Create a new set that is not a subset:\n";
@@ -48,37 +47,37 @@ int main()
      //     set3.add("Not");
      //     set3.add("subset");
 
-     //     cout << "isSubset(set3): returns " << set1.isSubset(set3)
+     //     cout << "isSubset(set3): returns " << intLS.isSubset(set3)
      //          << "; should return 0 (false)\n\n";
 
-     //     cout << "remove(\"is\"): returns " << set1.remove("is")
+     //     cout << "remove(\"is\"): returns " << intLS.remove("is")
      //          << "; should return 1 (true)\n\n";
 
-     //     cout << "remove(\"C++\"): returns " << set1.remove("C++")
+     //     cout << "remove(\"C++\"): returns " << intLS.remove("C++")
      //          << "; should return 0 (false)\n\n";
 
      //     cout << "Create a union between two sets:\n";
      //     ArraySet<string> unionSet(10);  // more than enough room for the union
-     //     set1.unionSet(set2, unionSet);
+     //     intLS.unionSet(set2, unionSet);
 
      //     cout << "unionSet(set2, unionSet): results in unionSet containing "
      //          << unionSet << "; should contain {This, is, a, set, test}\n\n";
 
      //     cout << "Create an intersection between two sets:\n";
      //     ArraySet<string> interSet(10); // more than enough room for the union
-     //     set1.intersection(set2, interSet);
+     //     intLS.intersection(set2, interSet);
 
      //     cout << "intersection(set2, interSet): results in interSet containing "
      //          << interSet << "; should contain {This, a}\n\n";
 
-     //     cout << "Create a vector from set1:\n";
-     //     vector<string> vect1 = set1.vector();
+     //     cout << "Create a vector from intLS:\n";
+     //     vector<string> vect1 = intLS.vector();
 
-     //     cout << "size() == vect1.size(): returns " << (set1.size() == vect1.size())
+     //     cout << "size() == vect1.size(): returns " << (intLS.size() == vect1.size())
      //          << "; should return 1 (true)\n\n";
 
      //     cout << "The intersection of two sets is a subset of both sets:\n"
-     //          << "isSubset(interSet): returns " << set1.isSubset(interSet)
+     //          << "isSubset(interSet): returns " << intLS.isSubset(interSet)
      //          << "; should be 1 (true)\n\n";
 
      //     cout << "A union of two sets is the superset of those two sets:\n"
