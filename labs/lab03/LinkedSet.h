df5a67bf70aa1bb6047d9d9d0a42c47fd4f03776
jay ess <mentoce@gmail.com>
2019-04-30 13:24:26 -0700

    Start work on lab 3

---
+++ labs/lab03/LinkedSet.h
@@ -0,0 +1,59 @@
+#ifndef _LINKED_SET_H
+#define _LINKED_SET_H
+
+#include "Node.h"
+#include "SetInterface.h"
+
+template<class T>
+class LinkedSet: public SetInterface<T>
+{
+public:
+    LinkedSet() {}
+
+    /** Destructor */
+    ~LinkedSet(){}
+    /** Adds an element to the set, if possible
+    @post the element is added to the set and the number of elements is
+      increased by one (if successful)
+   @param elem the object to be added as a new element of the set
+   @return true if the element was added, false if it wasn't */
+    bool add(const T &elem) {}
+
+    /** Returns the the intersection of this set and aSet
+    @param aSet the set to form an intersection with this set
+   @return The integer number of elements in the set */
+    void intersection(const SetInterface<T> &aSet, SetInterface<T> &unionSet) {}
+
+    /** Checks if an object is an element to the set
+    @param elem the object to be checked if it is an element of the set
+   @return true if elem is an element of this set, false if not */
+    bool isElement(const T &elem) {}
+
+    /** Returns the the intersection of this set and aSet
+    @param aSet the set to check if it is a subset of this set
+   @return true if aSet is a subset of this set, false if not */
+    bool isSubset(const SetInterface<T> &aSet) {}
+
+    /** Removes the object from the set, if it is present
+    @post the element is removed from the set and the number of elements is
+      decreased by one (if successful)
+   @param elem the object to be removed as a new element of the set
+   true if the element was removed, false if it wasn't */
+    bool remove(const T &elem) {}
+
+    /** Returns the integer number of elements in the set
+    @return The integer number of elements in the set */
+    int size() const {}
+
+    /** Returns the the union of this set and aSet
+    @param aSet the set to form a union with this set
+   @return The integer number of elements in the set */
+    void unionSet(const SetInterface<T> &aSet, SetInterface<T> &unionSet) {}
+
+    /** Returns all of the elements in the set as a vector
+    @return A vector containing the elements of the set */
+    std::vector<T> vector() const {}
+};
+
+#include "LinkedSet.cpp"
+#endif // _LINKED_SET_H
---
+++ labs/lab03/arraySetTest.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "ArraySet.h"
+
+using namespace std;
+
+int main()
+{
+    ArraySet<string> set1(7);
+
+    cout << "Testing the Array-based Set:\n"
+         << "---------------------------\n\n"
+         << "The initial set is empty.\n";
+
+    cout << "size(): returns " << set1.size()
+         << "; should be 0\n\n";
+
+    cout << "Add 5 elements to the set:\n";
+
+    string elements[] = {"This", "is", "a", "test", "set"};
+    for (int i = 0; i < 5; i++)
+    {
+        set1.add(elements[i]);
+    }
+    cout << "size(): returns " << set1.size()
+         << "; should be 5\n\n";
+
+    cout << "isElement(\"This\"): returns " << set1.isElement("This")
+         << "; should be 1 (true)\n\n";
+
+    cout << "isElement(\"C++\"): returns " << set1.isElement("C++")
+         << "; should be 0 (false)\n\n";
+
+    cout << "Create a new set that is a subset:\n";
+
+    ArraySet<string> set2;
+    for (int i = 0; i < 3; i++)
+    {
+        set2.add(elements[i]);
+    }
+
+    cout << "isSubset(set2): returns " << set1.isSubset(set2)
+         << "; should return 1 (true)\n\n";
+
+    cout << "Create a new set that is not a subset:\n";
+    ArraySet<string> set3(2);
+    set3.add("Not");
+    set3.add("subset");
+
+    cout << "isSubset(set3): returns " << set1.isSubset(set3)
+         << "; should return 0 (false)\n\n";
+
+    cout << "remove(\"is\"): returns " << set1.remove("is")
+         << "; should return 1 (true)\n\n";
+
+    cout << "remove(\"C++\"): returns " << set1.remove("C++")
+         << "; should return 0 (false)\n\n";
+
+    cout << "Create a union between two sets:\n";
+    ArraySet<string> unionSet(10);  // more than enough room for the union
+    set1.unionSet(set2, unionSet);
+
+    cout << "unionSet(set2, unionSet): results in unionSet containing "
+         << unionSet << "; should contain {This, is, a, set, test}\n\n";
+
+    cout << "Create an intersection between two sets:\n";
+    ArraySet<string> interSet(10); // more than enough room for the union
+    set1.intersection(set2, interSet);
+
+    cout << "intersection(set2, interSet): results in interSet containing "
+         << interSet << "; should contain {This, a}\n\n";
+
+    cout << "Create a vector from set1:\n";
+    vector<string> vect1 = set1.vector();
+
+    cout << "size() == vect1.size(): returns " << (set1.size() == vect1.size())
+         << "; should return 1 (true)\n\n";
+
+    cout << "The intersection of two sets is a subset of both sets:\n"
+         << "isSubset(interSet): returns " << set1.isSubset(interSet)
+         << "; should be 1 (true)\n\n";
+
+    cout << "A union of two sets is the superset of those two sets:\n"
+         << "unionSet.isSubset(set2): returns " << unionSet.isSubset(set2)
+         << "; should be 1 (true)";
+
+    return 0;
+}
