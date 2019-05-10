e0c83efb856faa41793a02f564c961565dc70c29
jay ess <mentoce@gmail.com>
2019-05-09 17:41:31 -0700

    Start definition of Maze class

--- labs/lab04/Maze.h
+++ labs/lab04/Maze.h
@@ -1,11 +1,23 @@
 #ifndef _MAZE_H
 #define _MAZE_H
 
+#include <vector>
 #include "Cell.h"
+#include "Grid.h"
 
 class Maze
 {
+public:
+    // Constructor
+    Maze(int, int);
 
+    // Destructor
+    ~Maze();
+private:
+    int numCols;
+    int numRows;
+
+    Grid grid;
 };
 
 #endif // _MAZE_H
