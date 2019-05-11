500204f13cc5d2325f9e97469883d2756d0ef749
Jay Ess <mentoce@gmail.com>
2019-05-10 22:05:21 -0700

    Add method to initialize grid with outer cells marked as occupied (walls)

--- labs/lab04/Maze.cpp
+++ labs/lab04/Maze.cpp
@@ -8,6 +8,7 @@ Maze::Maze(int c, int r, bool randStart)
     {
         randomStart();
     }
+    initGrid();
 }
 
 // Destructor
@@ -18,7 +19,20 @@ Maze::~Maze()
 
 void Maze::initGrid()
 {
+    std::vector<std::vector<Cell>> gVector = grid.vector();
 
+    for (int i = 0; i < grid.getNumRows(); i++)
+    {
+        for (int j = 0; j < grid.getNumCols(); j++)
+        {
+            if (j == 0 || i == 0 || j == (grid.getNumCols() - 1) || i == (grid.getNumRows() - 1))
+            {
+                Cell c = gVector[j][i];
+                c = cell::State::OCCUPIED;
+                grid.setCell(c);
+            }
+        }
+    }
 }
 
 void Maze::randomStart()
