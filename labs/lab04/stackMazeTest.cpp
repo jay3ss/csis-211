dd5b61bcffd44d37f2b4bffdb43aee1d08dceb7d
Jay Ess <mentoce@gmail.com>
2019-05-11 10:53:39 -0700

    Change method of printing, add tests for setting/getting end point

--- labs/lab04/stackMazeTest.cpp
+++ labs/lab04/stackMazeTest.cpp
@@ -7,11 +7,25 @@ using namespace std;
 int main()
 {
     // Maze maze(3, 3);
-    Maze maze(37, 42);
+    Maze maze(42, 37);
 
     Grid grid = maze.getGrid();
+    maze.setStart(3, 5);
+    maze.setEnd(39, 22);
 
-    cout << "The grid is\n\n";
+    cout << "The maze is\n\n";
 
-    cout << grid;
+    cout << maze << "\n\n";
+
+    const int *start = maze.getStart();
+    const int *end = maze.getEnd();
+
+    cout << "Starting coordinate is (" << start[0] << ", " << start[1] << ")\n";
+    cout << "Ending coordinate is (" << end[0] << ", " << end[1] << ")\n\n";
+
+    // Grid grid = maze.getGrid();
+    grid = maze.getGrid();
+    Cell c = grid.getCellAt(start[0], start[1]);
+
+    cout << "Now the maze looks like\n\n" << maze << endl;
 }
