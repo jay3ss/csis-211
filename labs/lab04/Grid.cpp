74a0eb6bfda56deb518d5e536a729c4c2c20c19b
Jay Ess <mentoce@gmail.com>
2019-05-12 21:37:49 -0700

    Update Grid class's docs

--- labs/lab04/Grid.cpp
+++ labs/lab04/Grid.cpp
@@ -17,17 +17,19 @@ Cell Grid::getCellAt(int x, int y)
     return cells[x][y];
 }
 
-// Checks if a cell is out of bounds
+// Checks if a cell is out of the bounds of the grid
 bool Grid::isOutOfBounds(int x, int y)
 {
     return (x < 0 || x > numCols || y < 0 || y > numRows);
 }
 
+// Initializes the grid, called in the constructor
 void Grid::init()
 {
     resize(numCols, numRows);
 }
 
+// Resizes the grid to the desired number of columns and rows
 void Grid::resize(int c, int r)
 {
     numCols = c;
@@ -48,11 +50,13 @@ void Grid::resize(int c, int r)
     cells = newCellsVect;
 }
 
+// Replaces the current cell in the grid with the passed in cell
 void Grid::setCell(Cell c)
 {
     cells[c.getXPosition()][c.getYPosition()] = c;
 }
 
+// Overloaded << operator to allow easy printing of Grid objects
 std::ostream &operator<<(std::ostream &strm, const Grid &obj)
 {
     for (int i = 0; i < obj.numRows; i++)
