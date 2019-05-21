0be7f6ca688aac6653cb4d322d3f2e924fe17615
jay ess <mentoce@gmail.com>
2019-05-20 21:29:15 -0700

    Add list interface definitions

--- labs/lab05/ListInterface.h
+++ labs/lab05/ListInterface.h
@@ -4,7 +4,52 @@
 template<class T>
 class ListInterface
 {
+	/** Destructor */
+	virtual ~ListInterface();
 
+	/** Sees if the list is empty
+	@return True if the list is empty, otherwise false. */
+	virtual bool empty() const = 0;
+
+	/** Get the number of entries in the list.
+	 @return The integer number of entries in the list. */
+	virtual int length() const = 0;
+
+	/** Insert an entry at a given position in the list.
+	@post A new entry will be in the list in the given position if
+		1 <= position <= length(). All entries after the insertion point will
+		be moved back by one.
+	@param position
+	@param newEntry
+	@return */
+	virtual bool insert(int position, T &newEntry) = 0;
+
+	/** Remove the entry at a given position.
+	@post The entry at the given position will no longer exist at that position
+		if 1 <= position <= length and all entries after the removal point will
+		be moved forward by one.
+	@param position	The position of the item to be removed.
+	@return If there is entry at the given position exists it will be removed
+		and the function will return true. If there is no entry at the given
+		position then false will be returned. */
+	virtual bool remove(int position) = 0;
+
+	/** Remove all entries from the list.
+	@post The list will be empty. */
+	virtual void clear() = 0;
+
+	/** Get the entry at a given position.
+	@pre An entry should be at the given position (i.e.,
+		1 <= position <= length())
+	@return The entry at the given position. */
+	virtual T entry(int position) const = 0;
+
+	/** Replace the entry at the given position.
+	@pre 1 <= position <= length()
+	@post The previous entry will be replaced with the new entry.
+	@param position The position in the list of the entry to be replaced.
+	@param newEntry The replacement entry */
+	virtual void replace(int position, T &newEntery) = 0;
 };
 
 #endif // _LIST_INTERFACE_H_
