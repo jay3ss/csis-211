e462e19401a2271d2ca8370e6445a504d32e5591
Jay Ess <mentoce@gmail.com>
2019-05-24 09:48:49 -0700

    Finish implementing TodoItem class'

--- labs/lab05/TodoItem.cpp
+++ labs/lab05/TodoItem.cpp
@@ -1,9 +1,33 @@
 #include "TodoItem.h"
 
-TodoItem::TodoItem()
+TodoItem::TodoItem(std::string i, item::status s) : item_(i), status_(s)
 {
 }
 
 TodoItem::~TodoItem()
 {
+}
+
+/** Returns the item */
+std::string TodoItem::item()
+{
+    return item_;
+}
+
+/** Sets the item */
+void TodoItem::item(std::string i)
+{
+    item_ = i;
+}
+
+/** Returns the status of the todo item */
+item::status TodoItem::status()
+{
+    return status_;
+}
+
+/** Sets the status of the todo item */
+void TodoItem::status(item::status s)
+{
+    status_ = s;
 }
--- labs/lab05/TodoItem.h
+++ labs/lab05/TodoItem.h
@@ -1,6 +1,8 @@
 #ifndef _TODO_ITEM_H_
 #define _TODO_ITEM_H_
 
+#include <string>
+
 namespace item
 {
     enum class status { COMPLETE, INPROGRESS };
@@ -9,10 +11,31 @@ namespace item
 class TodoItem
 {
 private:
-    item::status status_;
+    std::string item_;      // Text of what to do
+    item::status status_;   // Current status of the time
 public:
-    TodoItem();
+    /** Constructor */
+    TodoItem(std::string i = "", item::status = item::status::INPROGRESS);
+
+    /** Destructor */
     ~TodoItem();
+
+    /** Returns the item
+    @return The item */
+    std::string item();
+
+    /** Sets the item
+    @post The item will be set to the value passed in
+    @param i The new item */
+    void item(std::string i);
+
+    /** Returns the status of the todo item
+    @return The status of the item */
+    item::status status();
+
+    /** Sets the status of the todo item
+    @param s The new status of the item */
+    void status(item::status s);
 };
 
 #endif // _TODO_ITEM_H_
