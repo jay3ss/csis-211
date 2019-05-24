e9047dc295490edca2c47f8fef99505031ce9685
Jay Ess <mentoce@gmail.com>
2019-05-23 23:21:03 -0700

    Define basic methods

--- labs/lab05/TodoList.h
+++ labs/lab05/TodoList.h
@@ -9,8 +9,26 @@ class TodoList
 private:
     LinkedList<std::string> list;
 public:
+    /** Constructor */
     TodoList();
+
+    /** Constructor */
     ~TodoList();
+
+    /** Adds an item to the end of the todo list */
+    bool add();
+
+    /** Inserts an item to the desired position in the todo list */
+    bool insert();
+
+    /** Removes an item from the todo list */
+    bool remove();
+
+    /** Moves an existing item to a new position in the list */
+    bool move();
+
+    /** Marks item as either in progress or complete */
+    bool markAs();
 };
 
 #endif // _TODO_LIST_H_
