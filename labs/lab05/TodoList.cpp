37ff1490df5ca543f953c43b910c83b721afe915
Jay Ess <mentoce@gmail.com>
2019-05-24 11:54:07 -0700

    Write method stubs

--- labs/lab05/TodoList.cpp
+++ labs/lab05/TodoList.cpp
@@ -7,3 +7,33 @@ TodoList::TodoList()
 TodoList::~TodoList()
 {
 }
+
+/** Adds an item to the end of the todo list. */
+bool TodoList::add(const TodoItem &anItem)
+{
+}
+
+/** Returns a todo list item. */
+TodoItem TodoList::item(int position)
+{
+}
+
+/** Inserts an item to the desired position in the todo list. */
+bool TodoList::insert(int position, const TodoItem &anItem)
+{
+}
+
+/** Removes an item from the todo list. */
+bool TodoList::remove(int position)
+{
+}
+
+/** Moves an existing item to a new position in the list. */
+bool TodoList::move(int from, int to)
+{
+}
+
+/** Marks item as either in progress or complete. */
+void TodoList::markAs(int position, item::status s)
+{
+}
