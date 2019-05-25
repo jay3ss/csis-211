374ba3feea7719f157079c5e84d02f1d0f9a7105
jay ess <mentoce@gmail.com>
2019-05-25 16:25:43 -0700

    Add test for default behavior for todo list

--- labs/lab05/test-todo-list.cpp
+++ labs/lab05/test-todo-list.cpp
@@ -3,7 +3,38 @@
 #include "TodoList.h"
 
 
-TEST_CASE("blah", "[blah-blah]")
+TEST_CASE("Test behavior of newly a created list", "[TodoList]")
 {
+    TodoList todo;
 
+    SECTION("The list has length of zero")
+    {
+        REQUIRE(todo.length() == 0);
+    }
+
+    SECTION("Cannot remove items from an empty list")
+    {
+        REQUIRE_FALSE(todo.remove(5));
+    }
+
+    SECTION("Cannot move items in an empty list")
+    {
+        REQUIRE_FALSE(todo.move(5, 10));
+    }
+
+    TodoItem item;
+    item.item("Testing");
+    SECTION("Adding an item increases the list's length by one")
+    {
+        int origLength = todo.length();
+        REQUIRE(todo.add(item));
+        REQUIRE((origLength + 1) == todo.length());
+    }
+
+    SECTION("The item was removed successfully")
+    {
+        REQUIRE(todo.add(item));
+        REQUIRE(todo.remove(1));
+        REQUIRE(todo.length() == 0);
+    }
 }
