c5db16477bfcd888bb42435e1d95f68aa40cb2cf
jay ess <mentoce@gmail.com>
2019-05-25 23:05:42 -0700

    Write test suite for TodoList class

--- labs/lab05/test-todo-list.cpp
+++ labs/lab05/test-todo-list.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include "catch/catch.hpp"
 #include "TodoItem.h"
 #include "TodoList.h"
@@ -36,5 +37,57 @@ TEST_CASE("Test behavior of newly a created list", "[TodoList]")
         REQUIRE(todo.add(item));
         REQUIRE(todo.remove(1));
         REQUIRE(todo.length() == 0);
+    }
+}
+
+TEST_CASE("Working with a populated todo list", "[TodoList]")
+{
+    TodoList todo;
+    TodoItem item;
+    int numToAdd = 5;
+    for (int position = 1; position <= numToAdd; position++)
+    {
+        item.item("Item #" + std::to_string(position));
+        todo.add(item);
+    }
+
+    SECTION("The correct number of items were added")
+    {
+        REQUIRE(numToAdd == todo.length());
+    }
+
+    SECTION("The correct number of items are removed")
+    {
+        todo.remove(1);
+        todo.remove(2);
+        REQUIRE((numToAdd - 2) == todo.length());
+    }
+
+    SECTION("An item is moved to the correct place")
+    {
+        TodoItem item1("Item #1");
+        TodoItem item3("Item #3");
+        TodoItem temp;
+
+        // Move item 3 to the 1 position
+        REQUIRE(todo.move(3, 1));
+
+        // Check that item 3 moved to position 1
+        temp = todo.item(1);
+        REQUIRE(temp == item3);
+
+        // Check that item 1 moved to position 2
+        temp = todo.item(2);
+        REQUIRE(temp == item1);
+    }
+
+    SECTION("The list can change an item's status")
+    {
+        // Default status for an item is INPROGRESS, so change it
+        todo.markAs(1, item::status::COMPLETE);
+
+        // Get the item and that the item's status has changed
+        TodoItem temp = todo.item(1);
+        REQUIRE(item::status::COMPLETE == temp.status());
     }
 }
